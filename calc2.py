db375deddeb5b009c01348645d86e1de61158d16
Ramteja3597 <ramteja.janaki1997@gmail.com>
2020-03-04 19:14:00 +0530

    this is basic required calc files

---
+++ calc2.py
@@ -0,0 +1,20 @@
+def mod(a,b):
+    mod=a%b
+    return mod
+
+def hw():
+    return "hello world"
+
+def evenodd(a):
+    if a%2==0:
+        return "even"
+    else: return "odd"
+
+def fact(a):
+    if a==0 or a==1:
+        return 1
+    else :
+        return a*fact(a-1)
+
+
+
